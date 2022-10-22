04bb07cbf012695fa8900b303cd58afe5ebaefd6
edi-blackcode <edilerodrigues89@gmail.com>
2022-10-21 21:42:52 -0300

    first commit

---
+++ cookbook.rb
@@ -0,0 +1,64 @@
+INSERIR_RECEITA = 1
+VISUALIZAR_RECEITAS = 2
+BUSCAR_RECEITAS = 3
+SAIR = 4
+
+def bem_vinda
+    puts "Bem-vinda ao Cookbook, sua rede social de receitas"
+end
+
+def menu()
+    puts "[#{INSERIR_RECEITA}] Cadastrar uma receita"
+    puts "[#{VISUALIZAR_RECEITAS}] Ver todas as receitas"
+    puts "[#{BUSCAR_RECEITAS}] Buscar receitas"
+    puts "[#{SAIR}] Sair"
+    
+    print "Escolha uma opção: "
+    return gets.to_i()
+end
+
+def inserir_receita()
+    puts "Digite o nome da receita: "
+    nome = gets.chomp()
+    puts "Digite o tipo da receita: "
+    tipo = gets.chomp()
+    puts
+    puts "Receita de #{nome} cadastrada com sucesso!"
+    puts
+    return { nome: nome, tipo: tipo }
+end
+
+def imprimir_receitas(receitas)
+    puts "============ Receitas cadastradas ============"
+    receitas.each do |receita|
+    puts "#{receita[:nome]} - #{receita[:tipo]}"
+    end
+    puts
+    if receitas.empty?
+        puts "Nenhuma receita cadastrada"
+    end
+end
+
+bem_vinda()
+
+receitas = []
+
+opcao = menu()
+
+loop do
+    if(opcao == INSERIR_RECEITA)
+    receitas << inserir_receita() 
+    elsif(opcao == VISUALIZAR_RECEITAS)
+       imprimir_receitas(receitas)
+    elsif(opcao == SAIR)
+       break
+    else
+        puts "Opção inválida!"
+    end
+       
+    opcao = menu()
+end
+
+puts
+puts 'Obrigado por usar o Cookbook, até logo.'
+
